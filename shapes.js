b1dccf2c67234fcb0ff180e0f8fc2a28b1f58369
Yuliya Kaloshych <Yuliya_Kaloshych@epam.com>
2017-04-21 15:38:53 +0300

    bar chart creation

---
+++ shapes.js
@@ -0,0 +1,20 @@
+var dataArray = [5, 11, 18];
+
+var svg = d3.select('body').append('svg')
+    .attr('height', '100%')
+    .attr('width', '100%');
+
+svg.selectAll('rect') //find ? return it : return empty selections
+    .data(dataArray)//append data into selections
+    .enter().append('rect')
+    .attr('height', function(d, i){
+        return d*15;
+    })
+    .attr('width', '50')
+    .attr('x', function(d, i) {//5, 1
+        return 60*i; 
+    })
+    .attr('y', function(d, i){
+        return 300-(d*15);
+    })
+    .attr('fill', 'pink');
