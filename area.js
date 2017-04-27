bb51de80482233a4477e5a406eea868b7098c2e3
Yuliya Kaloshych <Yuliya_Kaloshych@epam.com>
2017-04-27 16:02:12 +0300

    Add x- axis

--- area.js
+++ area.js
@@ -1,6 +1,7 @@
 var dataArray = [15, 25, 85, 42, 7, 15, 42, 92, 6, 34, 51, 14, 2, 7, 65, 22, 17];
 var dataYears = ['2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016'];
 
+var parseDate = d3.timeParse('%Y');
 var height = 200;
 var width = 500;
 
@@ -12,18 +13,23 @@ var margin = {
 }
 
 var y = d3.scaleLinear()
-            .domain([0, 100]) // min and max for the real data
+            .domain([0, d3.max(dataArray)]) // min and max for the real data
             .range([height, 0]);
+var x = d3.scaleTime()
+    .domain(d3.extent(dataYears, function(d){ return parseDate(d)}))//extent function return array of min & max
+    .range([0, width]);
 
 var yAxis = d3
     .axisLeft(y)// labels come to the left
     .ticks(3)
     .tickPadding(10)
     .tickSize(10);//live only 3 labels(differs from version to version of d3)
+var xAxis = d3
+    .axisBottom(x);
 
 var area = d3.area()
                 .x(function(d, i) {
-                    return i* 20;
+                    return x(parseDate(dataYears[i]));
                 })
                 .y0(height)
                 .y1(function(d){
@@ -37,3 +43,7 @@ chartGroup.append('path').attr('d', area(dataArray));
 chartGroup.append("g")
     .attr("class", "axis y")
     .call(yAxis);
+chartGroup.append("g")
+    .attr("class", "axis x")
+    .attr('transform', 'translate(0,' + height + ')')
+    .call(xAxis);
