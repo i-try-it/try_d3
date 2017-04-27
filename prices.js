7205a67b4591f2ccc868895c01f0e1552399a96e
Yuliya Kaloshych <Yuliya_Kaloshych@epam.com>
2017-04-27 17:27:33 +0300

    draw line chart using csv

--- prices.js
+++ prices.js
@@ -1,13 +1,66 @@
 var parseDate = d3.timeParse('%m/%d/%Y');
 
 d3.csv('prices.csv')
-    .row(function(d) {
+    .row(function (d) {
         return {
             month: parseDate(d.month),
-            price:  Number(d.price.trim().slice(1))
+            price: Number(d.price.trim().slice(1))
         };
     })
-    .get(function(error, data) {
+    .get(function (error, data) {
         console.log(data);
-        console.log(error);
+        var height = 300;
+        var width = 500;
+        var max = d3.max(data, function (d) {
+            return d.price;
+        });
+        console.log(max);
+        var minDate = d3.min(data, function (d) {
+            return d.month;
+        });
+        console.log(minDate);
+        var maxDate = d3.max(data, function (d) {
+            return d.month;
+        });
+        console.log(maxDate);
+        var y = d3.scaleLinear()
+            .domain([0, max])
+            .range([height, 0]);
+        var x = d3.scaleTime()
+            .domain([minDate, maxDate])
+            .range([0, width])
+
+        var yAxis = d3.axisLeft(y);
+        var xAxis = d3.axisBottom(x);
+
+        var svg = d3.select('body')
+            .append('svg')
+            .attr('height', '100%')
+            .attr('width', '100%');
+        var margin = {
+            top: 40,
+            right: 50,
+            bottom: 0,
+            left: 50
+        };
+
+        var chartGroup = svg.append('g')
+            .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+        var line = d3.line()
+            .x(function (d) {
+                return x(d.month);
+            })
+            .y(function (d) {
+                return y(d.price);
+            });
+        chartGroup.append('path')
+            .attr('d', line(data));
+        chartGroup.append('g')
+            .attr('class', 'x axis')
+            .attr('transform', 'translate(0,' + height + ')')
+            .call(xAxis);
+        chartGroup.append('g')
+            .attr('class', 'y axis')
+            .call(yAxis);
+
     });
