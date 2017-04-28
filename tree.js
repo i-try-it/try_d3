f154919551bd9e7a63374c04d18ff2e31e0e9566
Yuliya Kaloshych <Yuliya_Kaloshych@epam.com>
2017-04-28 11:34:40 +0300

    curvy line

--- tree.js
+++ tree.js
@@ -41,7 +41,7 @@ d3.json('treeData.json').get(function(error, data) {
         .enter().append('path')
             .attr('class', 'link')
             .attr('d', function(d) {
-                return "M" + d.x + "," + d.y + "L" + d.parent.x + ","+ d.parent.y;
+                return "M" + d.x + "," + d.y + "C" + d.x + ","+ (d.parent.y + d.y)/2 + " "+ d.parent.x + ","+ (d.parent.y + d.y)/2 + " " + d.parent.x + "," + d.parent.y;
             })            
     
 })
